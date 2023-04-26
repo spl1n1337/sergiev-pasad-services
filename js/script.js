b314be4efb8c7d89449d6371b5ffe7ab9e2dd627
spl1n1337 <panov1337.lp@gmail.com>
2023-04-26 13:47:06 +0400

    production version

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.querySelector('.menu__mobile'),
-          headerBot = document.querySelector('.header__bot');
+  const menuIcon = document.querySelector('.menu__mobile'),
+        headerBot = document.querySelector('.header__bot');
 
 
-    menuIcon.addEventListener('click', () => {
-        menuIcon.classList.toggle('mob__active');
-        headerBot.classList.toggle('header__active');
-    });
+  menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('mob__active');
+      headerBot.classList.toggle('header__active');
+  });
 
 
-    const form = document.querySelector('form'),
-          thankyou = document.querySelector('.thankyou');
+  const form = document.querySelector('form'),
+        thankyou = document.querySelector('.thankyou');
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const phoneInput = form.querySelector('[name="client-phone"]');
-        const phoneRegex = /^((\+7|7|8)+([0-9]){10})$/;
-        if(!phoneRegex.test(phoneInput.value)){
-            alert("Пожалуйста, введите корректный телефонный номер РФ");
-            return;
-        }
-
-    try {
-        const response = await fetch("mail.php", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams(new FormData(form)).toString(),
-        });
-        const data = await response.json();
-        
-        if (data.status === "success") {
-          thankyou.style.display = "block";
-          console.log(data.status);
-          setTimeout(() => {
-            thankyou.style.display = "none";
-            form.reset();
-          }, 2000);
-        } else {
-          alert(data.message);
-        }
-      } catch (error) {
-        console.error(error);
-        alert("Ошибка при отправке формы");
+  form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      const phoneInput = form.querySelector('[name="client-phone"]');
+      const phoneRegex = /^((\+7|7|8)+([0-9]){10})$/;
+      if(!phoneRegex.test(phoneInput.value)){
+          alert("Пожалуйста, введите корректный телефонный номер РФ");
+          return;
       }
-    });
+
+  try {
+      const response = await fetch("mail.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams(new FormData(form)).toString(),
+      });
+      const data = await response.json();
+      
+      if (data.status === "success") {
+        thankyou.style.display = "block";
+        console.log(data.status);
+        setTimeout(() => {
+          thankyou.style.display = "none";
+          form.reset();
+        }, 2000);
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Ошибка при отправке формы");
+    }
+  });
 });
